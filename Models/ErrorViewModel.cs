35679345c2a5b8a73606fa97bc594cce61353a65
Adrian Farnell <adrianjfarnell@gmail.com>
2022-05-06 13:53:25 +0100

    feat: first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace githubActionsPlay.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
